1c9c65463bb2341931c1e51907beab4e9894f15a
Pl4gue <david10608@gmail.com>
2017-12-26 19:30:03 +0100

    Exercise 5.7: TODO(2)- display weather info extra , Send Data

--- S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -1,17 +1,27 @@
 package com.example.android.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
     private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+
+    private TextView mWeatherForDay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        // TODO (2) Display the weather forecast that was passed from MainActivity
+        // COMPLETED (2) Display the weather forecast that was passed from MainActivity
+        mWeatherForDay = (TextView) findViewById(R.id.tv_weatherForDay);
+
+        Intent parent = getIntent();
+        if (parent.hasExtra(Intent.EXTRA_TEXT)) {
+            mWeatherForDay.setText(parent.getStringExtra(Intent.EXTRA_TEXT));
+        }
     }
 }
